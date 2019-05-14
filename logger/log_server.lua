8a469acaf258e2ed713f70facb0be3a89686966e
Nathan C <39100023+nathanctech@users.noreply.github.com>
2019-05-13 23:54:00 -0400

    Big update to solve some issues with logging
    Sometimes it will record the wrong leave times for players. This has been fixed.
    
    On resource start this will ensure no leave dates are NULL (which should only happen on server crash) to keep consistency when reporting.

--- logger/log_server.lua
+++ logger/log_server.lua
@@ -1,30 +1,42 @@
 server_name = GetConvar("server_num", "UNK") -- should be a CONVAR in server.cfg
 
+playerList = {}
+
+AddEventHandler("onResourceStart", function(name)
+    if name == GetCurrentResourceName() then
+        -- update player without a leave date, server must have crashed
+        local data = {}
+        data["server"] = server_name
+        MySQL.Async.execute("update player_log set leave_date = NOW() where server_name = @server and leave_date IS NULL", data)
+    end
+end)
+
 RegisterNetEvent("Log:Joined")
 AddEventHandler('Log:Joined', function()
     playerId = source
     local data = {}
-    data["@discord"] = nil
-    data["@license"] = nil
-    data["@steam"] = nil
-    data["@ip"] = nil
-    for k, v in pairs(GetPlayerIdentifiers(source)) do
+    data["discord"] = nil
+    data["license"] = nil
+    data["steam"] = nil
+    data["ip"] = nil
+    for k, v in pairs(GetPlayerIdentifiers(playerId)) do
         identifier = stringsplit(v, ':')
         data[identifier[1]] = identifier[2]   
     end
-    data["@server_name"] = server_name
-    data["@server_id"] = playerId
-    data["@user_name"] = GetPlayerName(source)
-    MySQL.Async.execute("update player_log set leave_date = NOW() where server_name = @server_name and fivem_license = @license and leave_date is null", data)
+    data["server_name"] = server_name
+    data["server_id"] = playerId
+    data["user_name"] = GetPlayerName(playerId)
+    playerList[playerId] = data["license"]
     MySQL.Async.execute("insert into player_log (server_name, server_id, user_name, fivem_license, steam_hex, discord_id, ip, join_date) values (@server_name, @server_id, @user_name, @license, @steam, @discord, @ip, NOW())", data)
 end)
 
 AddEventHandler("playerDropped", function(player, disconnectReason)
+    local playerId = source
     local data = {}
-    playerId = source
     data["server_name"] = server_name
     data["server_id"] = playerId
-    MySQL.Async.execute("update player_log set leave_date = NOW() where server_name = @server_name and server_id = @server_id", data)
+    data["license"] = playerList[playerId]
+    MySQL.Async.execute("update player_log set leave_date = NOW() where server_name = @server_name and server_id = @server_id and fivem_license = @license and leave_date IS NULL", data)
 end)
 
 function stringsplit(inputstr, sep)
